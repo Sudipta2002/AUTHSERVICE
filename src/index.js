5161ad35cfd5b15ecb134f0b60b2161e6062f575
Sudipta Ghosh <sudiptaghosh27september@gmail.com>
2022-12-26 08:22:35 +0530

    Roles added

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 const { PORT } = require('./config/serverConfig');
 const apiRoutes = require('./routes/index');
+const db = require('./models/index');
 // const UserRepository = require('./repository/user-repository');
 // const UserService = require('./services/user-service');
 const prepareAndStartServer = async() => {
@@ -20,6 +21,9 @@ const prepareAndStartServer = async() => {
     // const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InN1ZGlwdGFnaG9zaDI3c2VwdGVtYmVyQGdtYWlsLmNvbSIsImlkIjoxLCJpYXQiOjE2NzE5NTk5MzEsImV4cCI6MTY3MTk2NzEzMX0.2XYBav1DCYjLw9aGs5GqP8OpG7KZ2QN_BTyR6VNVl8k';
     // const response = service.verifyToken(token);
     // console.log(response);
+    // if (process.env.DB_SYNC) {
+    //     db.sequelize.sync({ alter: true });
+    // }
     app.listen(PORT, () => {
         console.log(`Server is running in port ${PORT}`);
     })
